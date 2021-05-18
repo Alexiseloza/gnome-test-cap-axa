0bad6ad8dcb6743b78499afe9c79f568022d4b4c
Alexis Edgar Loza <47640237+Alexiseloza@users.noreply.github.com>
2021-05-18 09:48:23 -0300

    Add files via upload
    test axa

---
+++ src/components/Landing/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './index.sass'
+import { useHistory } from 'react-router-dom'
+
+export default function Landing() {
+    const history = useHistory()
+
+    const onMainPage = () => {
+        history.push('search')
+    }
+
+    return  <> 
+        <section className="landing">
+            <div className="landing__container container" onClick={event => {
+                event.preventDefault()
+                onMainPage()
+                }}>
+                <h1 className="container__title">Welcome to Test</h1>                
+            </div>
+         </section>
+    </>
+}
